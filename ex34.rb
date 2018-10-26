2a76e09090b65f509aee1342fcbac5dce89950ea
Gary Bunofsky <gary@garybunofsky.com>
2018-10-26 16:30:46 -0400

    ex34

---
+++ ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animals[1]}: Answer ruby"
+puts "The third (3rd) animal is #{animals[2]}: Answer peacock"
+puts "The first (1st) animal is #{animals[0]}: Answer bear"
+puts "The animal at 3 #{animals[3]}: Answer kangaroo"
+puts "The fifth animal #{animals[4]}: Answer whale"
+puts "The animal at #{animals[2]}: Answer peacock"
+puts "The sixth animal #{animals[5]}: Answer platypus"
+puts "The animal at 4. #{animals[4]}: Answer whale"
